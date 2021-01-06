7751bf3d3f22d0fe356e14362aec92e7880ddcbd
Anna Witek <46248723+annwit@users.noreply.github.com>
2021-01-06 09:58:42 +0100

    First commit

---
+++ codility_1.1_Binary_gap.py
@@ -0,0 +1,19 @@
+def solution(n):
+    zero_count = 0
+    result = 0
+    while n != 0 and n % 2 == 0:  # Skipping the tailing zero
+        n = n // 2
+    while n != 0:
+        if n % 2 == 0:
+            zero_count += 1
+            result = max(zero_count, result)
+        else:
+
+            zero_count = 0
+
+        n = n // 2
+
+    return result
+
+
+print(solution(1041))
